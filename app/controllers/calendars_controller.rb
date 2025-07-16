692387070152cbcdb4f2697e2eabaf03f81673d2
tener9-kt <kym.sunny21@gmail.com>
2025-07-16 14:55:41 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,14 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = Date.today.wday  # ← 曜日番号を取得（0:日〜6:土）
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+      wday = wdays[wday_num] # ← 曜日文字列に変換
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[x] }
       @week_days.push(days)
     end
 
